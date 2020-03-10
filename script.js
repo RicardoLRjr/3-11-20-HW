7b2300efcd3992acb2963111e128e96010bbffc5
Ricardo Russ <ricardoruss@live.com>
2020-03-10 19:06:52 -0400

    added in basic layout for title page, next task is to work in script.js file to create functionality of button moving

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pseudocode:
+// create nav bar with name of game, option to view scores, and remaining time.
+// create title page with button to start game
+// create hidden <div></div> with text that shows wrong or right depending on answer
+// include functionality so that button answers change
+// code individual question buttons, with preventing restart functions
+// include ticking down clock in right corner, time taken with wrong answer
+// when question is wrong, take time away and display wrong
+//  
