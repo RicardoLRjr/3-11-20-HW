bc3dd6897435bf4a21652b5772d8417032f119f4
Ricardo Russ <ricardoruss@live.com>
2020-03-10 22:52:56 -0400

    added in last window functionality, still need to copy score over

--- script.js
+++ script.js
@@ -17,17 +17,24 @@
 
 // Variables:
 var seconds = 75;
+var time = document.getElementById("time")
 // this is the score/time element featured in the top right corner. It ticks down at rate of one per second
 // and will go down by 10 immediately if question is wrong.
 var questionArray = [question1, question2, question3, question4];
 // this is the array containing each of the question objects. 
-var startBtn = document.getElementById("start-button")
-var startPage = document.getElementById("start-page")
-var questionDiv = document.getElementById("question-div")
-var hiddenDiv = document.getElementById("correct-wrong")
-
-
-
+var startBtn = document.getElementById("start-button");
+// refers to the start button
+var startPage = document.getElementById("start-page");
+// refers to the start page
+var questionDiv = document.getElementById("question-div");
+// refers to the div containing the question elements
+var hiddenDiv = document.getElementById("correct-wrong");
+// refers to the div containing the correct/wrong dialogue
+var scorescreen = document.getElementById("score-screen")
+// refers to the score-screen dialogue
+var submitScore = document.getElementById("score-submit")
+// refers to the submit-score button
+var NameValue = document.getElementById("exampleNameInput")
 // Objects:
 var question1 = {
     True1:"Javascript",
@@ -61,9 +68,46 @@ var question4 = {
 
 // Functions:
 
-startBtn.addEventListener("click", function(){
-    startPage.setAttribute("style", "display: none")
+localStorage.getItem(NameValue)
+
+function setTime() {
+    time.textContent = seconds
+    var timerInterval = setInterval(function() {
+      seconds--;
+      time.textContent = "Time: " + seconds;
+  
+      if(seconds === 0) {
+        clearInterval(timerInterval);
+        console.log("time ran out!");
+        questionDiv.setAttribute("style", "display: none")
+        hiddenDiv.setAttribute("style", "display: none") 
+        scorescreen.setAttribute("style", "display: block")
+      }
+  
+    }, 100);
+  }
+// Modified from activity  04-09
+
+startBtn.addEventListener("click", function() {
+    startPage.setAttribute("style", "display: none");
+
+    questionDiv.setAttribute("style", "display: inline");
+    hiddenDiv.setAttribute("style", "display: inline");
+
     
-    questionDiv.textContent = "this is a test of question div"
-    hiddenDiv.textContent = "this is a test of hidden div"
+
+    setTime();
+    questionGame();
+    
+})
+
+function questionGame() {
+  
+
+    for (i = 0; i < questionArray.length; i++) {
+        console.log("questionarray");
+}}
+submitScore.addEventListener("click", function(event) {
+event.preventDefault();
+localStorage.setItem("Name Value", NameValue.value,);
 })
